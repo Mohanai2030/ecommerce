44410009a382932334ffecda8cdeb6c83bfe7c9d
Mohana Krishnan <mohanakrishnanv18@gmail.com>
2026-03-08 15:33:18 +0530

    added update delivery status routes

--- server/router/deliverypartnerRouter.js
+++ server/router/deliverypartnerRouter.js
@@ -1,6 +1,56 @@
 import express from 'express';
+import { deliverypartnerAuthorization } from '../middleware/auth';
+import { prisma } from '../lib/prisma';
+
 const deliverypartnerRouter = express.Router();
 
+deliverypartnerRouter.use(deliverypartnerAuthorization)
 
+deliverypartnerRouter.put('outForDelivery',async(req,res)=>{
+    const {assignedOrderId} = req.params;
+
+    try{
+        await prisma.assignedorder.update({
+            where:{
+                assignedorderid:assignedOrderId,
+                deliverypartnerid:req.id,
+            },
+            data:{
+                ordertable:{
+                    update:{
+                        orderstatus:'outfordelivery'
+                    }
+                }
+            }
+        });
+
+        return res.status(200)
+    }catch(err){
+        return res.status(500)
+    }
+})
+
+deliverypartnerRouter.put('delivered',async(req,res)=>{
+    const {assignedOrderId} = req.params;
+
+    try{
+        await prisma.assignedorder.update({
+            where:{
+                assignedorderid:assignedOrderId,
+                deliverypartnerid:req.id,
+            },
+            data:{
+                ordertable:{
+                    update:{
+                        orderstatus:'delivered'
+                    }
+                }
+            }
+        })
+        return res.status(200)
+    }catch(err){
+        return res.status(500)
+    }
+})
 
 export {deliverypartnerRouter};
