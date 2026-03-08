1dd79a45785922820f550c54c2d4bea73d1f9a3e
Mohana Krishnan <mohanakrishnanv18@gmail.com>
2026-03-08 20:34:17 +0530

    moved router files into proper directories

--- server/router/admin/adminRouter.js
+++ server/router/admin/adminRouter.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import { prisma } from '../lib/prisma.js';
-import { roles } from '../constants/roles.js';
-import { accessTokenSign, refreshTokenSign } from '../services/jwt.js';
+import { prisma } from '../../lib/prisma.js';
+import { roles } from '../../constants/roles.js';
+import { accessTokenSign, refreshTokenSign } from '../../services/jwt.js';
 import cookieParser from 'cookie-parser';
 const adminRouter = express.Router();
 
