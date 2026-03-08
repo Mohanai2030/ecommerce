eaceafe837755aece3cd938fd9bf959c10b2ae5a
Mohana Krishnan <mohanakrishnanv18@gmail.com>
2026-03-08 20:40:39 +0530

    corrected the admin and deliverypartner routers

--- server/router/admin/adminRouter.js
+++ server/router/admin/adminRouter.js
@@ -3,8 +3,9 @@ import { prisma } from '../../lib/prisma.js';
 import { roles } from '../../constants/roles.js';
 import { accessTokenSign, refreshTokenSign } from '../../services/jwt.js';
 import cookieParser from 'cookie-parser';
+import { adminAuthRouter } from './adminauthRouter.js';
 const adminRouter = express.Router();
 
-
+adminRouter.use(adminAuthRouter);
 
 export {adminRouter};
--- server/router/deliverypartner/deliverypartnerRouter.js
+++ server/router/deliverypartner/deliverypartnerRouter.js
@@ -3,8 +3,11 @@ import { prisma } from '../../lib/prisma.js';
 import { roles } from '../../constants/roles.js';
 import { accessTokenSign, refreshTokenSign } from '../../services/jwt.js';
 import cookieParser from 'cookie-parser';
+import { deliveryPartnerAuthRouter } from './deliverypartnerauthRouter.js';
 const deliverypartnerRouter = express.Router();
 
+
+deliverypartnerRouter.use(deliveryPartnerAuthRouter);
 
 
 export {deliverypartnerRouter};
